3229b1beaf53fefd0f98383dd3b9c8f2819a307f
Ikars <ikars.melnalksnis@esynergy.lv>
2025-06-28 15:44:40 +0300

    Fix xdebug not working.

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Run migrations
+php artisan migrate --force
+
 # Set permissions for Laravel directories
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
